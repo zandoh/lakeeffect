297bab6625023c19f6e992af99b0a448183b533b
Zac Clark <spamdoh9@gmail.com>
2020-03-30 20:36:11 -0400

    Adds files from setup

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Header from "../components/Header";
+
+const About: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <p>About</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello Next.js</p>
+    </div>
+  );
+};
+
+export default Home;
