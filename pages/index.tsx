efe79d5dbe17ba3e5484ed2dff9be1c34461e8a6
Zac Clark <spamdoh9@gmail.com>
2020-04-16 17:42:07 -0400

    Dev: adds yeti logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 import Header from "../components/Header";
+import YetiSvg from "../components/svg/Yeti";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
       <p>Hello Next.js</p>
+      <YetiSvg height={250} width={250} color={"blue"} />
     </div>
   );
 };
