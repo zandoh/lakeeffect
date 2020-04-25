0c8764ac2f97822a7b85429a7866128b8eba4fdd
Zac Clark <zandoh9@gmail.com>
2020-04-25 16:01:04 -0400

    Dev: initial file dump

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const path = require("path");
+const { PATHS } = require(path.normalize(`${__dirname}/const.js`));
+
 module.exports = {
   exportTrailingSlash: true,
-  exportPathMap: function() {
+  exportPathMap: function () {
     return {
-      "/": { page: "/" }
+      [`${PATHS.ROOT}`]: { page: `${PATHS.ROOT}` },
+      [`${PATHS.INFO}`]: { page: `${PATHS.INFO}` },
+      [`${PATHS.APPLY}`]: { page: `${PATHS.APPLY}` },
+      [`${PATHS.PROGRESS}`]: { page: `${PATHS.PROGRESS}` },
+      [`${PATHS.RECRUITMENT}`]: { page: `${PATHS.RECRUITMENT}` },
+      [`${PATHS.NEEDS}`]: { page: `${PATHS.RECRUITMENT}` },
     };
-  }
+  },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,54 @@
 import { NextPage } from "next";
-import Header from "../components/Header";
+import Link from "next/link";
 import YetiSvg from "../components/svg/Yeti";
+import { PATHS } from "../const";
+import styled from "styled-components";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Header />
-      <p>Hello Next.js</p>
-      <YetiSvg height={250} width={250} color={"blue"} />
-    </div>
+    <StyledHomeWrapper>
+      <StyledTitle>Lake Effect</StyledTitle>
+      <StyledLinkGroup>
+        <Link href={PATHS.INFO}>
+          <StyledLink>Info</StyledLink>
+        </Link>
+        <Link href={PATHS.APPLY}>
+          <StyledLink>Apply</StyledLink>
+        </Link>
+      </StyledLinkGroup>
+      <YetiSvg height={250} width={250} bodyColor={"#FFF"} eyeColor={"#FFF"} />
+      <StyledLinkGroup>
+        <Link href={PATHS.PROGRESS}>
+          <StyledLink>Progress</StyledLink>
+        </Link>
+        <Link href={PATHS.RECRUITMENT}>
+          <StyledLink>Recruitment</StyledLink>
+        </Link>
+      </StyledLinkGroup>
+    </StyledHomeWrapper>
   );
 };
+
+const StyledLinkGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const StyledHomeWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
+  justify-content: center;
+`;
+
+const StyledLink = styled.a`
+  margin-right: 15px;
+`;
+
+const StyledTitle = styled.h1`
+  font-family: ${(props) => props.theme.brand.fonts.baloo.bold};
+`;
 
 export default Home;
