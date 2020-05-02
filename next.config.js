32f59591ae73526209df697d4c55729bc72c644c
Zac Clark <zandoh9@gmail.com>
2020-05-02 03:22:01 -0400

    Master: removes unused routes in config

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ module.exports = {
   exportPathMap: function () {
     return {
       [`${PATHS.ROOT}`]: { page: `${PATHS.ROOT}` },
-      [`${PATHS.INFO}`]: { page: `${PATHS.INFO}` },
-      [`${PATHS.APPLY}`]: { page: `${PATHS.APPLY}` },
-      [`${PATHS.PROGRESS}`]: { page: `${PATHS.PROGRESS}` },
-      [`${PATHS.RECRUITMENT}`]: { page: `${PATHS.RECRUITMENT}` },
-      [`${PATHS.NEEDS}`]: { page: `${PATHS.RECRUITMENT}` },
     };
   },
 };
