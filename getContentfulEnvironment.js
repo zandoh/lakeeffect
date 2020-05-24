537900452a3d9d841db3a431b9d7a290233553db
Zac Clark <spamdoh9@gmail.com>
2020-05-24 17:09:51 -0400

    Dev: Adds typescript gen for contentful types

---
+++ getContentfulEnvironment.js
@@ -0,0 +1,11 @@
+const contentfulManagement = require("contentful-management");
+
+module.exports = function () {
+  const contentfulClient = contentfulManagement.createClient({
+    accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+  });
+
+  return contentfulClient
+    .getSpace(process.env.CONTENTFUL_SPACE)
+    .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT));
+};
